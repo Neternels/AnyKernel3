44186f941d00de63a606b6cd254a937d8c469169
MrRob0-X <hanya8493@gmail.com>
2022-07-25 01:12:36 +0000

    Anykernel: Configure mi439

--- anykernel.sh
+++ anykernel.sh
@@ -4,23 +4,23 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=NetErnels kernel
+kernel.string=
 do.devicecheck=1
 do.modules=0
-do.systemless=1
+do.systemless=0
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
-device.name4=tuna
-device.name5=
+device.name1=olive
+device.name2=olivelite
+device.name3=olivewood
+device.name4=pine
+device.name5=mi439
 supported.versions=
 supported.patchlevels=
 '; } # end properties
 
 # shell variables
-block=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
+block=/dev/block/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
 patch_vbmeta_flag=auto;
@@ -40,25 +40,57 @@ set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 ## AnyKernel boot install
 dump_boot;
 
-# begin ramdisk changes
+ui_print "Mounting /vendor..."
+mount -o rw,remount /vendor
 
-# init.rc
-backup_file init.rc;
-replace_string init.rc "cpuctl cpu,timer_slack" "mount cgroup none /dev/cpuctl cpu" "mount cgroup none /dev/cpuctl cpu,timer_slack";
+vndk_version=$(file_getprop /vendor/build.prop ro.vendor.build.version.sdk)
 
-# init.tuna.rc
-backup_file init.tuna.rc;
-insert_line init.tuna.rc "nodiratime barrier=0" after "mount_all /fstab.tuna" "\tmount ext4 /dev/block/platform/omap/omap_hsmmc.0/by-name/userdata /data remount nosuid nodev noatime nodiratime barrier=0";
-append_file init.tuna.rc "bootscript" init.tuna;
+if [ $vndk_version -lt 30 ]; then
+    abort "Unsupported VNDK version. Aborting..."
+fi
 
-# fstab.tuna
-backup_file fstab.tuna;
-patch_fstab fstab.tuna /system ext4 options "noatime,barrier=1" "noatime,nodiratime,barrier=0";
-patch_fstab fstab.tuna /cache ext4 options "barrier=1" "barrier=0,nomblk_io_submit";
-patch_fstab fstab.tuna /data ext4 options "data=ordered" "nomblk_io_submit,data=writeback";
-append_file fstab.tuna "usbdisk" fstab;
+ui_print "Detecting WiFi HAL..."
 
-# end ramdisk changes
+wifi_hal=$(find /vendor/lib64 /vendor/lib -name "libwifi-hal.so" | head -n 1)
+if grep -q "pronto_wlan.ko" $wifi_hal; then
+    abort "Unsupported WiFi HAL. Aborting..."
+fi
+
+ui_print "Detecting camera HAL..."
+
+if [[ ! -z "$(find /vendor/lib64 /vendor/lib -name '*lib2d*')" ]]; then
+    patch_cmdline "prebuilt_camera_hal" "prebuilt_camera_hal"
+else
+    patch_cmdline "prebuilt_camera_hal" ""
+fi
+
+flash_boot;
+
+ui_print " "
+
+ui_print "Mounting /system..."
+mount -o rw,remount /system
+
+## Patches
+# Prevent init from overriding kernel tweaks.
+ui_print "Patching system's init..."
+# IMO this is kinda destructive but works
+find /system/etc/init/ -type f | while read file; do 
+sed -Ei 's;[^#](write /proc/sys/(kernel|vm)/(sched|dirty|perf_cpu|page-cluster|stat|swappiness|vfs));#\1;g' $file
+done
+# IORap
+ui_print "Patching system's build.prop..."
+patch_prop /system/build.prop "ro.iorapd.enable" "true"
+patch_prop /system/build.prop "iorapd.perfetto.enable" "true"
+patch_prop /system/build.prop "iorapd.readahead.enable" "true"
+patch_prop /system/build.prop "persist.device_config.runtime_native_boot.iorap_readahead_enable" "true"
+patch_prop /system/build.prop "persist.device_config.runtime_native_boot.iorap_perfetto_enable" "true"
+# Prevent init from overriding ZRAM algorithm
+ui_print "Patching vendor's init..."
+remove_line "/vendor/etc/init/hw/init.qcom.rc" "comp_algorithm" "global"
+# Replace post_boot with ours.
+ui_print "Pushing init.qcom.post_boot.sh..."
+replace_file "/vendor/bin/init.qcom.post_boot.sh" "0755" "init.qcom.post_boot.sh"
 
 write_boot;
 ## end boot install
